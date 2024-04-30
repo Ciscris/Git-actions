8a262f76bf5ee99cfad6584f97eda1d44366b0c0
MIMOAC <mimoac8@gmail.com>
2024-04-30 13:50:41 -0600

    el bueno?

---
+++ .github/workflows/act.js
@@ -0,0 +1,15 @@
+// printInfo.js
+
+// Tu nombre completo
+const fullName = "Cristian Diaz de Leon";
+
+// Versión de Node.js utilizada
+const nodeVersion = process.version;
+
+// Tu expediente
+const expediente = "7152332";
+
+// Imprimir la información
+console.log(`Nombre completo: ${fullName}`);
+console.log(`Versión de Node.js: ${nodeVersion}`);
+console.log(`Expediente: ${expediente}`);
